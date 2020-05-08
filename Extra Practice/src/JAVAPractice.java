41ccf839bd8365bc05e7d818635e8c26811481d7
Desmond Young <Desmond.Young25@yahoo.com>
2020-05-08 11:04:32 -0700

    Shine bright like a diamond

---
+++ Extra Practice/src/JAVAPractice.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class JAVAPractice {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello! How many stars would you like? ");
+		Scanner sc = new Scanner(System.in);
+		int numOfStars = sc.nextInt();
+
+		for(int i = 1; i <= numOfStars; i++) {
+ 			for(int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		for (int i = numOfStars - 1; i > 0; i--) {
+			for(int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}	
+}
